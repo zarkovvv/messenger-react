09b785d3752269ffa15c3921bebd579664dcf499
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-06-06 17:12:49 +0300

    added mobile view functionality

--- src/components/Sidebar/Header/Header.js
+++ src/components/Sidebar/Header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Header.scss';
 import {Avatar, IconButton} from "@material-ui/core";
-import {RateReviewOutlined, Search} from "@material-ui/icons";
+import {Close, RateReviewOutlined, Search} from "@material-ui/icons";
 import {useSelector} from "react-redux";
 import {selectUser} from "../../../features/userSlice";
 import db, {auth} from "../../../firebase";
 import firebase from "firebase/app";
 
 const Header = () => {
+
+    const sidebarElement = document.getElementsByClassName('sidebar')[0];
+    const sidebarHeaderElement = document.getElementsByClassName('sidebar-header')[0];
+    const sidebarChatsElement = document.getElementsByClassName('sidebar-chats')[0];
+    const sidebarArrowElement = document.getElementById('arrow');
+    const sidebarCloseElement = document.getElementById('close');
+
 
     const user = useSelector(selectUser);
 
@@ -28,6 +35,14 @@ const Header = () => {
        await auth.signOut();
     }
 
+    function closeSidebar() {
+        sidebarElement.classList.remove('clicked');
+        sidebarHeaderElement.classList.remove('clicked');
+        sidebarChatsElement.classList.remove('clicked');
+        sidebarArrowElement.classList.remove('clicked');
+        sidebarCloseElement.classList.remove('clicked');
+    }
+
     return (
         <div className="sidebar-header">
 
@@ -40,6 +55,9 @@ const Header = () => {
 
             <IconButton variant="outlined"  onClick={createChat}>
                 <RateReviewOutlined className="create-chat"/>
+            </IconButton>
+            <IconButton id="close" onClick={closeSidebar}>
+                <Close />
             </IconButton>
 
         </div>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,10 +3,17 @@ import './Sidebar.scss';
 import Header from "./Header/Header";
 import Chat from "./Chat/Chat";
 import db from "../../firebase";
+import {ArrowForwardIos} from "@material-ui/icons";
+import {IconButton} from "@material-ui/core";
 
 const Sidebar = () => {
 
     const [chats, setChats] = useState([]);
+    const sidebarElement = document.getElementsByClassName('sidebar')[0];
+    const sidebarHeaderElement = document.getElementsByClassName('sidebar-header')[0];
+    const sidebarChatsElement = document.getElementsByClassName('sidebar-chats')[0];
+    const sidebarArrowElement = document.getElementById('arrow');
+    const sidebarCloseElement = document.getElementById('close');
 
     useEffect(() => {
         db.collection('chats').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
@@ -17,12 +24,23 @@ const Sidebar = () => {
         });
     }, []);
 
+    function handleClick() {
+        sidebarElement.classList.add('clicked');
+        sidebarHeaderElement.classList.add('clicked');
+        sidebarChatsElement.classList.add('clicked');
+        sidebarArrowElement.classList.add('clicked');
+        sidebarCloseElement.classList.add('clicked');
+    }
+
     return (
         <div className="sidebar">
             <Header />
             <div className="sidebar-chats">
                 {chats.map(({id, data: {chatName, owner}}) => (<Chat key={id} id={id} chatName={chatName} owner={owner}/>))}
             </div>
+            <IconButton id="arrow" onClick={handleClick}>
+                <ArrowForwardIos />
+            </IconButton>
         </div>
     );
 };
